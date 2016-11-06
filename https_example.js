d61b7a7db16a7eefdffd567fab890cf922a02ae1
Joao Coelho <joaocoelho93@gmail.com>
2016-11-06 02:09:28 +0000

    HAAAAALP

--- https_example.js
+++ https_example.js
@@ -1,5 +1,5 @@
-var https = require('https');
 
+ var https = require('https');
 
 var options = {
   host: 'www.google.ca',
@@ -7,8 +7,13 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
